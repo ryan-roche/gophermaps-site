2f218bd9fd1e290e45f595e44356eb0919e73966
Ryan <roche189@umn.edu>
2025-01-26 21:07:03 -0600

    finished download page

--- pages/download/index.tsx
+++ pages/download/index.tsx
@@ -1,18 +1,19 @@
-import { Divider } from "@heroui/divider";
-import { Card } from "@heroui/card";
+import { Card, CardBody } from "@heroui/card";
 import { Image } from "@heroui/image";
 
 import { subtitle, title } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
+import { appCodes } from "./appCodes";
+import { Link } from "@heroui/link";
 
 export default function DocsPage() {
   return (
     <DefaultLayout>
       {/* Page Title */}
-      <section className="flex flex-col items-center justify-center gap-4 md:py-10">
+      <section className="flex flex-col items-center justify-center gap-4 pb-4">
         <div className="inline-block md:max-w-none text-center justify-center">
-          <span className={title()}>Download&nbsp;</span>
-          <span className={title({ color: "yellow" })}>GopherMaps</span>
+          <span className={`${title()}`}>Download&nbsp;</span>
+          <span className={`${title({ color: "yellow" })}`}>GopherMaps</span>
         </div>
       </section>
 
@@ -20,25 +21,42 @@ export default function DocsPage() {
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <span className={`${subtitle()} inline-block text-center`}>
           Scan the QR code for your respective platform using your mobile device
+          <br /> (Android version still in development)
         </span>
       </section>
 
       {/* Platform QR Codes */}
-      <section className="flex items-center justify-center gap-4 py-8 md:py-10">
-        <Image alt={"App Store QR code"} src={"/qr-appstore.svg"} width={300} />
-        <Divider orientation="vertical" />
-        <Image
-          alt={"Google Play Store QR code"}
-          src={"/qr-playstore-placeholder.svg"}
-          width={300}
-        />
+      <section className={"flex flex-row items-center justify-center gap-16"}>
+        {appCodes.map((item) => (
+          <Card
+            key={item.title}
+            isBlurred
+            className={"border"}
+            isDisabled={!item.isAvailable}
+            isPressable={item.isAvailable}
+            shadow={"sm"}
+            onPress={() => {
+              window.location.replace(item.link);
+            }}
+          >
+            <CardBody>
+              <div>
+                <Image
+                  className={!item.isAvailable ? "blur z-0" : "z-0"}
+                  src={item.imgSrc}
+                  width={300}
+                />
+              </div>
+            </CardBody>
+          </Card>
+        ))}
       </section>
 
       {/* Lower subtitle */}
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <span className={`${subtitle()} inline-block text-center`}>
           Alternatively, visit this page on your device to be redirected
-          automatically.
+          automatically
         </span>
       </section>
     </DefaultLayout>
