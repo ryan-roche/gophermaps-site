48aaf8aa6d40e9105feffd04b9961348ef865ffd
Ryan <roche189@umn.edu>
2025-01-28 17:44:34 -0600

    First draft done

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,19 +6,23 @@ import { Image } from "@heroui/image";
 
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
 import DefaultLayout from "@/layouts/default";
 
 export default function IndexPage() {
   return (
     <DefaultLayout>
-      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <section className="flex flex-col items-center justify-center gap-4 pb-8">
         <div className="inline-block max-w-xl text-center justify-center">
-          <img alt={"GopherMaps word mark"} src="/wordmark-white.svg" />
+          <img
+            alt={"GopherMaps word mark"}
+            src="/wordmark-white.svg"
+            width={500}
+          />
           <div className={subtitle({ class: "mt-4" })}>
             An interactive map for the Gopher Way
           </div>
         </div>
+        <Image className={""} src={"/mockups/multi.png"} />
       </section>
     </DefaultLayout>
   );
