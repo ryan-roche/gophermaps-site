e3d0124bdce4a7a5eaa6ef9aac5ac893f75bdba4
Ryan <roche189@umn.edu>
2025-01-29 13:06:55 -0600

    Remove unused imports

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,10 +4,6 @@ import {
   NavbarBrand,
   NavbarItem,
 } from "@heroui/navbar";
-import { Button } from "@heroui/button";
-import { Kbd } from "@heroui/kbd";
-import { Link } from "@heroui/link";
-import { Input } from "@heroui/input";
 import { link as linkStyles } from "@heroui/theme";
 import NextLink from "next/link";
 import clsx from "clsx";
