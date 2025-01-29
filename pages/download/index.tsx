ededa7d43fb9cdc2464454d2363dfd8287bebe86
Ryan <roche189@umn.edu>
2025-01-28 20:53:21 -0600

    Add dummy page

--- pages/download/index.tsx
+++ pages/download/index.tsx
@@ -1,64 +1,13 @@
-import { Card, CardBody } from "@heroui/card";
-import { Image } from "@heroui/image";
-
-import { subtitle, title } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
-import { appCodes } from "./appCodes";
-import { Link } from "@heroui/link";
 
-export default function DesktopDownloadPage() {
+// This page shouldn't ever be shown. middleware.tsx should "intercept" the
+// request and automatically give the platform-specific download subpage AS
+// the download page
+
+export default function DownloadPage() {
   return (
     <DefaultLayout>
-      {/* Page Title */}
-      <section className="flex flex-col items-center justify-center gap-4 pb-4">
-        <div className="inline-block md:max-w-none text-center justify-center">
-          <span className={`${title()}`}>Download&nbsp;</span>
-          <span className={`${title({ color: "yellow" })}`}>GopherMaps</span>
-        </div>
-      </section>
-
-      {/* Upper subtitle */}
-      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        <span className={`${subtitle()} inline-block text-center`}>
-          Scan the QR code for your respective platform using your mobile device
-          <br /> (Android version still in development)
-        </span>
-      </section>
-
-      {/* Platform QR Codes */}
-      <section className={"flex flex-row items-center justify-center gap-16"}>
-        {appCodes.map((item) => (
-          <Card
-            key={item.title}
-            isBlurred
-            className={"border"}
-            isDisabled={!item.isAvailable}
-            isPressable={item.isAvailable}
-            shadow={"sm"}
-            onPress={() => {
-              window.location.replace(item.link);
-            }}
-          >
-            <CardBody>
-              <div>
-                <Image
-                  className={!item.isAvailable ? "blur z-0" : "z-0"}
-                  src={item.imgSrc}
-                  width={300}
-                />
-              </div>
-            </CardBody>
-          </Card>
-        ))}
-      </section>
-
-      {/* Lower subtitle */}
-      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        <span className={`${subtitle()} inline-block text-center`}>
-          Alternatively, visit this page on your device to be redirected
-          automatically
-        </span>
-      </section>
+      <p>dummy</p>
     </DefaultLayout>
   );
 }
